6f9e5bb24f511761ea4f255e6f56a71c33796afb
Даниил Лямин <Niger120404@yandex.ru>
2022-12-24 23:35:22 +0300

    Классы раскиданы по файлам

---
+++ BinaryTreeEditor/BinaryTreeNode.cs
@@ -0,0 +1,16 @@
+﻿namespace BinaryTreeEditor
+{
+	internal class BinaryTreeNode
+	{
+		public int Value { get; set; }
+		
+		public BinaryTreeNode? Left { get; set; }
+		
+		public BinaryTreeNode? Right { get; set; }
+
+		public BinaryTreeNode(int value)
+		{
+			Value = value;
+		}
+	}
+}
